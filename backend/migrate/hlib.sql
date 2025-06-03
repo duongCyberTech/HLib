ae09bd2a498070b339ecb9c6d86defeeb43b24a7
duongCyberTech <maidangduong92tn@gmail.com>
2025-06-03 23:13:04 +0700

    update file sql

--- backend/migrate/hlib.sql
+++ backend/migrate/hlib.sql
@@ -8,19 +8,15 @@ create table users(
     mname nvarchar(20) not null,
     lname nvarchar(20) not null,
     username varchar(50) unique,
-    password varchar(50) not null,
+    password varchar(255) not null,
     email varchar(50) unique,
     avata varchar(100) ,
-    status smallint default 1, -- able (1), disable (0)
+    status bit default 0, -- able (1), disable (0)
     role varchar(10) default 'user',
     salary decimal(10,2)
 );
 
 
-alter table users 
-drop column password,
-add column password nvarchar(255);
-select*from users
 
 
 
