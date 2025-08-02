ea9379c79057837d06fc667fcb9b5f206ee97ee1
dCyberTech <maidangduong92tn@gmail.com>
2025-08-02 21:48:47 +0700

    Tạm commit để merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,20 @@ import Signup from './components/Signup';
 import Verify from './components/Verify';
 import Login from './components/Login';
 import ForgotPassword from './components/ForgotPassword';
+import Test from './pages/Test'
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
+            {/* Auth */}
             <Route path="/" element={<Signup />} />
             <Route path="/verify" element={<Verify />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             {/* Add more routes as needed */}
+            {/* Test */}
+            <Route path='/test' element={<Test />}/>
         </Routes>
     </BrowserRouter>
   );
