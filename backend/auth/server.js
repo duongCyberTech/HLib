140aa81e46cfa4d4b52f3030115a70d5471d1d54
dCyberTech <maidangduong92tn@gmail.com>
2025-08-03 17:04:40 +0700

    GENERATE ALL SERVICE

--- backend/auth/server.js
+++ backend/auth/server.js
@@ -4,30 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
-const swaggerUi = require("swagger-ui-express");
-const swaggerJSDoc = require("swagger-jsdoc");
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
-// Cấu hình swagger-jsdoc
-const swaggerSpec = swaggerJSDoc({
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Auth API",
-      version: "1.0.0",
-    },
-    servers: [{ url: "http://localhost:3001" }],
-  },
-  apis: ["./routes/*.js"], // 👈 đúng đường dẫn file chứa @swagger
-});
-console.log(JSON.stringify(swaggerSpec, null, 2));
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-// Example route
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello Swagger!" });
-});
 
 app.use(cors());
 app.use(bodyParser.json());
