1e8e59a3e09a6fe96d6ef9746fdf7ba6ef912882
nhanx04 <nhan.nguyencs04@gmail.com>
2025-09-21 20:16:36 +0700

    change layout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,49 +1,24 @@
 import { Box } from '@mui/material';
-import { Header, Footer, Sidebar } from '../components/common';
+import { Header, Sidebar } from '../components/common';
 
 export default function MainLayout({ children }) {
   return (
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-      width: '100vw',
-      margin: 0,
-      padding: 0
-    }}>
-      {/* Header */}
-      <Header />
-
-      {/* Main Content Area with Sidebar */}
-      <Box sx={{
-        display: 'flex',
-        flexGrow: 1,
-        overflow: 'hidden',
-        width: '100%'
-      }}>
-        {/* Sidebar */}
-        <Sidebar />
-
-        {/* Main Content */}
+    <Box sx={{ display: 'flex', height: '100vh', overflow: 'hidden' }}>
+      <Sidebar />
+      <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+        <Header />
         <Box
           component="main"
           sx={{
             flexGrow: 1,
             p: 3,
-            display: 'flex',
-            flexDirection: 'column',
             overflow: 'auto',
             backgroundColor: '#fafafa',
-            width: 'calc(100vw - 250px)',
-            minHeight: 'calc(100vh - 64px)' // Subtract header height
           }}
         >
           {children}
         </Box>
       </Box>
-
-      {/* Footer */}
-      <Footer />
     </Box>
   );
 }
